e3bfc30919ca2e33789698ece0df50e96b976bb5
Zaine Mendes <zainemendes@hotmail.com>
2024-10-19 15:39:33 -0300

    UserController

---
+++ src/main/java/com/una/linkhub/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.una.linkhub.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.una.linkhub.model.User;
+import com.una.linkhub.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+	
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> userList = service.findAll();
+		return ResponseEntity.ok().body(userList);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
+
+}
