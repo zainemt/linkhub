3acaa771366397e073ee0438ce8a29b427dcfec2
Zaine Mendes <zainemendes@hotmail.com>
2024-10-19 15:05:34 -0300

    criação do projeto

---
+++ src/main/java/com/una/linkhub/LinkhubApplication.java
@@ -0,0 +1,13 @@
+package com.una.linkhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/una/linkhub/LinkhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.una.linkhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
